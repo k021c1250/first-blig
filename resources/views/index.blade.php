43d30d9a36861f920cdcfbfef9b19d2a6ea8b29e
k021c1250 <k021c1250@g.neec.ac.jp>
2022-01-21 10:54:57 +0900

    (add)投稿一覧の表示

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,23 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Blog</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
 
        
-     
+    
     </head>
     <body>
         <h1>blog name</h1>
         <div class = 'posts'>
+            @foreach ($posts as $post)
             <div class = 'post'>
-                <h2 class = 'title'>Title</h2>
-                <p class = 'body'>This is a sample</p>
+                <h2 class = 'title'>{{$post->title}}</h2>
+                <p class = 'body'>{{$post->body}}</p>
             </div>
+            @endforeach
         </div> 
     </body>
 </html>
